e25c7f9ded3911876cc58e1c783ff40cf89198e1
Faitsumaru <kiryakov.g.m@gmail.com>
2024-02-28 11:13:05 +0300

    files settings

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ function scripts() {
 }
 
 function styles() {
-	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
+	return src([`app/${preprocessor}/*.*`, `!app/${preprocessor}/_*.*`])
 		.pipe(eval(`${preprocessor}glob`)())
 		.pipe(eval(preprocessor)({ 'include css': true }))
 		.pipe(postCss([
@@ -138,7 +138,7 @@ function deploy() {
 }
 
 function startwatch() {
-	watch([`app/styles/${preprocessor}/**/*`], { usePolling: true }, styles)
+	watch([`app/${preprocessor}/**/*`], { usePolling: true }, styles)
 	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
 	watch(['app/images/src/**/*'], { usePolling: true }, images)
 	watch([`app/**/*.{${fileswatch}}`], { usePolling: true }).on('change', browserSync.reload)
