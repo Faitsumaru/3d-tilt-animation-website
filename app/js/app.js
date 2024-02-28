dba8a5c130b60a0fc47a097ec5e3c86fa2d04863
Faitsumaru <kiryakov.g.m@gmail.com>
2024-02-28 13:21:52 +0300

    page loading gsap animation effects js

--- app/js/app.js
+++ app/js/app.js
@@ -31,4 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
         gsap.to('.content', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
     }
 
+    gsap.to('.card', { zoom: .98 })
+	gsap.to('.l_main', { opacity: 1, duration: .1 })
+	gsap.to('.l2_main', { opacity: 1, left: -15, top: 15, duration: .25, delay: .25 })
+	gsap.to('.l3_main', { opacity: 1, left: -30, top: 30, duration: .25, delay: .25 })
+	gsap.to('.card-russia', { opacity: .07, duration: .1 })
+	gsap.to('.card-logo_w', { opacity: 1, duration: .225 })
+	gsap.to('.card-chip', { opacity: 1, duration: .225 })
+	gsap.to('.card-valid', { opacity: 1, zoom: 1, duration: .1, delay: .25 })
+	gsap.to('.card-number-holder', { opacity: 1, zoom: 1, duration: .1, delay: .25 })
+
 })
